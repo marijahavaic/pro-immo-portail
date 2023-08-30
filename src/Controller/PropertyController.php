1318049063ad3e3caaa73089cf761f7d4d0e7917
Marija Havaic <marija.havaic@gmail.com>
2023-08-30 13:42:12 +0200

    Créer un controller Property

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    #[Route('/property', name: 'app_property')]
+    public function index(): Response
+    {
+        return $this->render('property/index.html.twig', [
+            'controller_name' => 'PropertyController',
+        ]);
+    }
+}
