97f74ac06528951a08b1116b2371009d72a90319
aurelieparsonneau <a.parsonneau@gmail.com>
2023-08-31 15:34:31 +0200

    Ajout de paginator

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(PropertyRepository $properties, Request $request): Response
+    public function index(PropertyRepository $properties, PaginatorInterface $paginator, Request $request): Response
     {
-        $properties = $properties->findAll();
+        $query = $properties->findAll();
+
+        $pagination = $paginator->paginate(
+            $query, // Requête contenant les données à paginer
+            $request->query->getInt('page', 1), // Numéro de la page en cours, 1 par défaut
+            10 // Nombre de résultats par page
+        );
+
         return $this->render('home/index.html.twig', [
-            'properties' => $properties,
+            'properties' => $pagination
         ]);
     }
-}
+    }
