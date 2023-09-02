eda79c187f1c6798e7a50d56f9cfebd9874454d4
Marija Havaic <marija.havaic@gmail.com>
2023-09-02 12:04:54 +0200

    Modif Favorite controller

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FavoriteController extends AbstractController
-{
+{  
+    
+
     #[Route('/favorite', name: 'app_favorite')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // Création d'un nouvel objet Favorite
+        $favorite = new Favorite();
+
+        // Ajout des relations
+        $favorite->addUser($user);
+        $favorite->addProperty($property);
+
+        // Enregistrement en base de données
+        $entityManager->persist($favorite);
+        $entityManager->flush();
         return $this->render('favorite/index.html.twig', [
             'controller_name' => 'FavoriteController',
         ]);
