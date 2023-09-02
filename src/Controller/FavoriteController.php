bc29cd6ba41dce643d7bcd6fb8b3bfc7e1fa8d3f
Marija Havaic <marija.havaic@gmail.com>
2023-09-02 15:21:47 +0200

    Réparer Favoris

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -2,32 +2,22 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FavoriteController extends AbstractController
 {  
-    
-
     #[Route('/favorite', name: 'app_favorite')]
-    public function index(EntityManagerInterface $entityManager): Response
-    {
-        $entityManager = $this->getDoctrine()->getManager();
+    public function index(): Response
+    {   
+        $user = $this->getUser();
+        $favoriteProperties = $user->getFavorite()->getValues();
 
-        // Création d'un nouvel objet Favorite
-        $favorite = new Favorite();
-
-        // Ajout des relations
-        $favorite->addUser($user);
-        $favorite->addProperty($property);
-
-        // Enregistrement en base de données
-        $entityManager->persist($favorite);
-        $entityManager->flush();
+        
         return $this->render('favorite/index.html.twig', [
-            'controller_name' => 'FavoriteController',
-        ]);
+            'favorites' => $favoriteProperties,
+            ]);
     }
 }
