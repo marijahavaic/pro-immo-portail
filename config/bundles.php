c375dd9903af52b774d6cbc53a4310b48986f472
aurelieparsonneau <a.parsonneau@gmail.com>
2023-09-05 15:13:55 +0200

    Ajout du stimulus

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Typed\TypedBundle::class => ['all' => true],
 ];
